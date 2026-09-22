603784662b7f5d0e7f5bc0d2efbd3abb697e355e
BastianRuiz <juanmunozr@unicauca.edu.co>
2026-09-22 14:12:34 -0500

    Agrega datos de prueba (seed) para la entidad Producto
    Se cargan 3 productos de ejemplo al iniciar la aplicación,
    facilitando las pruebas de los métodos GET, PUT y DELETE.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO productos (nombre, descripcion, precio, cantidad) VALUES ('Teclado mecanico', 'Teclado mecanico retroiluminado', 150000.0, 20);
+INSERT INTO productos (nombre, descripcion, precio, cantidad) VALUES ('Mouse inalambrico', 'Mouse inalambrico ergonomico', 65000.0, 35);
+INSERT INTO productos (nombre, descripcion, precio, cantidad) VALUES ('Monitor 24 pulgadas', 'Monitor Full HD 24 pulgadas', 480000.0, 10);
